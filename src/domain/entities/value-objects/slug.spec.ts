b02483e52a013bdc0c610e6bdd296f60d30b8720
Neto Rossetti <netinho.rossetti@gmail.com>
2023-12-11 17:58:39 -0300

    feat: conceito de value-objects
    Value objects são propriedades que possam ter regras especificas
    dentro de uma entidade, para este tipo de propriedades, o indicado é
    cria-las como value-objects para especificar sua regra

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("deve ser possivel criar um slug de um texto", () => {
+  const slug = Slug.createFromText("Teste de criação de uma slug.");
+
+  expect(slug.value).toEqual("teste-de-criacao-de-uma-slug");
+});
