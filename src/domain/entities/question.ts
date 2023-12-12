715ff9bf0b626d590b34e10e54901c5f6b4e829c
Neto Rossetti <netinho.rossetti@gmail.com>
2023-12-12 11:33:55 -0300

    feat: Classe base de entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from "node:crypto";
 import { Slug } from "./value-objects/slug";
+import { Entity } from "../../core/entities/entity";
 
 interface QuestionProps {
   title: string;
@@ -8,18 +8,4 @@ interface QuestionProps {
   authorId: string;
 }
 
-export class Question {
-  public id: string;
-  public title: string;
-  public content: string;
-  public slug: Slug;
-  public authorId: string;
-
-  constructor(props: QuestionProps, id?: string) {
-    this.id = id ?? randomUUID();
-    this.title = props.title;
-    this.authorId = props.authorId;
-    this.content = props.content;
-    this.slug = props.slug;
-  }
-}
+export class Question extends Entity<QuestionProps> {}
