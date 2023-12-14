950acc595468a47a15806e62e7dbd3c0ee61fb5a
Neto Rossetti <netinho.rossetti@gmail.com>
2023-12-14 14:38:36 -0300

    feat: Caso de uso: Deletar comentário da resposta

---
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
+import { makeAnswerComment } from "test/factories/make-answer-comment";
+import { DeleteAnswerCommentUseCase } from "./delete-answer-comment";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: DeleteAnswerCommentUseCase;
+
+describe("Delete Answer Comment", () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+    sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository);
+  });
+
+  it("should be able to delete a answer comment", async () => {
+    const newAnswerComment = makeAnswerComment({
+      authorId: new UniqueEntityId("author-1"),
+    });
+    await inMemoryAnswerCommentsRepository.create(newAnswerComment);
+
+    expect(inMemoryAnswerCommentsRepository.items).toHaveLength(1);
+
+    await sut.execute({
+      authorId: "author-1",
+      answerCommentId: newAnswerComment.id.toString(),
+    });
+
+    expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0);
+  });
+
+  it("not should be able to delete another author answer comment", async () => {
+    const newAnswerComment = makeAnswerComment({
+      authorId: new UniqueEntityId("author-1"),
+    });
+    await inMemoryAnswerCommentsRepository.create(newAnswerComment);
+
+    expect(async () => {
+      await sut.execute({
+        authorId: "author-2",
+        answerCommentId: newAnswerComment.id.toString(),
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -10,4 +10,21 @@ export class InMemoryAnswerCommentsRepository
   async create(answercomment: AnswerComment) {
     this.items.push(answercomment);
   }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id
+    );
+    if (itemIndex !== -1) {
+      this.items.splice(itemIndex, 1);
+    }
+  }
+
+  async findById(id: string): Promise<AnswerComment | null> {
+    const answerComment = this.items.find((item) => item.id.toString() === id);
+    if (!answerComment) {
+      return null;
+    }
+    return answerComment;
+  }
 }
