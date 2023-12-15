4b9ce343d401219e916b27b18e5eb99f8538e159
Neto Rossetti <netinho.rossetti@gmail.com>
2023-12-15 15:04:56 -0300

    feat: Teste de edição da pergunta

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -3,14 +3,22 @@ import { makeQuestion } from "test/factories/make-question";
 import { EditQuestionUseCase } from "./edit-question";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { makeQuestionAttachment } from "test/factories/make-question-attachment";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
 let sut: EditQuestionUseCase;
 
 describe("Edit Question", () => {
   beforeEach(() => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
-    sut = new EditQuestionUseCase(inMemoryQuestionsRepository);
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository();
+    sut = new EditQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionAttachmentsRepository
+    );
   });
 
   it("should be able to edit a question", async () => {
@@ -18,12 +26,23 @@ describe("Edit Question", () => {
       authorId: new UniqueEntityId("author-1"),
     });
     await inMemoryQuestionsRepository.create(newQuestion);
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId("1"),
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId("2"),
+      })
+    );
 
     await sut.execute({
       authorId: "author-1",
       questionId: newQuestion.id.toString(),
       title: "Novo Titulo",
       content: "Novo Conteudo",
+      attachmentsIds: ["1", "3"],
     });
 
     expect(inMemoryQuestionsRepository.items).toHaveLength(1);
@@ -31,6 +50,15 @@ describe("Edit Question", () => {
       title: "Novo Titulo",
       content: "Novo Conteudo",
     });
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems
+    ).toHaveLength(2);
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems
+    ).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId("3") }),
+    ]);
   });
 
   it("not should be able to edit a question from another author", async () => {
@@ -44,6 +72,7 @@ describe("Edit Question", () => {
       questionId: newQuestion.id.toString(),
       title: "Novo Titulo",
       content: "Novo Conteudo",
+      attachmentsIds: [],
     });
 
     expect(result.isFailure()).toBe(true);
